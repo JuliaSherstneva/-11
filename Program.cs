170aa718849227a652da246953b8ac22e0139e55
исп-22 <-22@LAB30-04>
2025-01-15 18:34:44 +0300

    Добавьте файлы проекта.

---
+++ Program.cs
@@ -0,0 +1,30 @@
+﻿/*4.Выполнить сортировку элементов, записанных на нечетных местах. 
+ Использовать сортировку простым выбором. */ 
+
+int[] array = { 12, 7, 18, 3, 25, 10, 30, 2, 45, 5 };
+
+for (int i = 0; i < array.Length; i++)
+{
+    Console.WriteLine(array[i] + " ");
+}
+Console.WriteLine();
+
+for (int i = 1; i < array.Length; i += 2) 
+{
+    int minIndex = i;
+    for (int j = i + 2; j < array.Length; j += 2)
+    {
+        if (array[j] < array[minIndex])
+        {
+            minIndex = j;
+        }
+    }
+    int temp = array[i];
+    array[i] = array[minIndex];
+    array[minIndex] = temp;
+}
+for (int i = 0; i < array.Length; i++)
+{
+     Console.Write(array[i] + " ");
+}
+Console.WriteLine();
